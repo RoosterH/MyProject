89defc612d9c20e0d326f8a02f4075e5881b8a19
Hung-Jen Hung <rc30rr@hotmail.com>
2021-01-17 18:26:44 -0800

    Change GOOGLE_MAP_API_KEY to GOOGLE_API_KEY also added GOOGLE_API_KEY_PROD in env-prod.json, production mode should use GOOGLE_API_KEY_PROD that restricts certain API calls.

--- BackEnd/util/location.js
+++ BackEnd/util/location.js
@@ -7,7 +7,7 @@ async function getCoordinatesForAddress(address) {
 	const response = await axios.get(
 		`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
 			address
-		)}&key=${process.env.GOOGLE_MAP_API_KEY}`
+		)}&key=${process.env.GOOGLE_API_KEY}`
 	);
 
 	const data = response.data;
